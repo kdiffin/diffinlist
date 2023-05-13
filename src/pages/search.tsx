b95afe6fe1621f5c43bfe49a144e35f9e5d11bc7
JeweIxx <vhiunese@gmail.com>
2023-05-13 11:51:59 +0400

    search page wip info added

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,7 +1,19 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import Button from "~/components/ui/Button";
 
 const search = () => {
-  return <div>search</div>;
+  return (
+    <div className="flex h-full flex-col items-center justify-center gap-6 p-2 text-center text-4xl ">
+      <p className="max-w-screen-sm italic text-neutral-300">
+        This page is a work in progress, please return to the home page.
+      </p>
+      <Button className="text-lg">
+        <Link href="/">Return home</Link>
+      </Button>
+    </div>
+  );
 };
 
 export default search;
